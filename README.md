b5bff43670cd3dc17ce0794796975465b9797fbe
Mackie Burgess <mackie.burgess@gmail.com>
2022-12-11 13:22:11 +0000

    QoL / neatness

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+To execute my solution code for a particular day `N` - `cargo run --bin dayN`.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!");
+    println!("To run a day of AOC solutions, run `cargo run --bin dayN` where N is the day number.");
 }
