ca4407aaee8c76b95f8f15b1bfc29717c5dd3ff1
Mackie Burgess <mackie.burgess@gmail.com>
2022-12-23 23:25:18 +0000

    day 23 - part 2

--- src/day23.rs
+++ src/day23.rs
@@ -1,5 +1,20 @@
+// TODO: it'd be nice to do a rewrite here
+// there's likely a lot of potential for code deduplication
+
 use std::collections::{VecDeque,HashSet};
 
+fn get_map() -> VecDeque<VecDeque<bool>> {
+    include_str!("../data/23.input")
+        .lines()
+        .map(|line| {
+            line.chars().map(|char| {
+                match char {
+                    '#' => true,
+                    _ => false
+                }
+            }).collect()
+        }).collect()
+}
 
 fn is_clear(map: &VecDeque<VecDeque<bool>>, point: (usize, usize), round: usize) -> (usize, usize) {
     let mut lonely: u8 = 0;
@@ -99,16 +114,7 @@ fn trim_map(mut map: VecDeque<VecDeque<bool>>) -> VecDeque<VecDeque<bool>> {
 }
 
 fn loose_elves() -> usize {
-    let mut map: VecDeque<VecDeque<bool>> = include_str!("../data/23.input")
-        .lines()
-        .map(|line| {
-            line.chars().map(|char| {
-                match char {
-                    '#' => true,
-                    _ => false
-                }
-            }).collect()
-        }).collect();
+    let mut map = get_map();
 
     for round in 0..10 {
         // trim and then expand the map
@@ -177,6 +183,77 @@ fn loose_elves() -> usize {
         }).sum()
 }
 
+fn loosened_elves() -> usize {
+    // basically just loose_elves() but exhaustive
+    let mut map = get_map();
+
+    for round in 0.. {
+        let mut moved = false;
+
+        map = trim_map(map);
+
+        map.push_front(VecDeque::new());
+        map.push_back(VecDeque::new());
+
+        for _ in 0..map[1].len() {
+            map.front_mut().expect("shh").push_back(false);
+            map.back_mut().expect("shh").push_back(false);
+        }
+
+        for row in map.iter_mut() {
+            row.push_back(false);
+            row.push_front(false);
+        }
+
+        let mut visited1: HashSet<(usize, usize)> = HashSet::new();
+        let mut visited2: HashSet<(usize, usize)> = HashSet::new();
+
+        // part one: monkey planning
+        let mut plans: Vec<Vec<Option<(usize, usize)>>> = vec![];
+
+        for y in 0..map.len() {
+            plans.push(vec![]);
+
+            for x in 0..map[y].len() {
+                if map[y][x] {
+                    let new_zone = is_clear(&map, (x, y), round);
+                    plans.last_mut().expect("shh").push(Some(new_zone));
+
+                    // check whether multiple elves are going to the same place
+                    if visited1.contains(&new_zone) {
+                        visited2.insert(new_zone);
+                    }
+
+                    visited1.insert(new_zone);
+                } else {
+                    plans.last_mut().expect("shh").push(None);
+                }
+            }
+        }
+
+        // part two: monkey moving
+        for y in 0..map.len() {
+            for x in 0..map[y].len() {
+                if let Some(plan) = plans[y][x] {
+                    // ensure no two plans go to the same place
+                    if !visited2.contains(&plan) {
+                        map[y][x] = false;
+                        map[plan.1][plan.0] = true;
+                        if (plan.0, plan.1) != (x, y) {
+                            moved = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if !moved { return round + 1; }
+    }
+
+    return 0;
+}
+
 fn main() {
     println!("part one: {}", loose_elves());
+    println!("part two: {}", loosened_elves());
 }
